8eb809f69a69606f03e70d38760a87aec523ae39
tbng84 <tbng84@gmail.com>
2024-07-31 08:08:40 +0700

    timer

--- pyrevm/test.py
+++ pyrevm/test.py
@@ -1,29 +1,30 @@
 import os
 from pyrevm import EVM, Env, BlockEnv
+import logging
 
 import sys # for testing
 sys.path.append('..')
 
-from helpers.utils import load_contract_bin, encode_address, encode_uint, func_selector
+from helpers import load_contract_bin, encode_address, encode_uint, func_selector, timer_decorator
 
 address = "0xA8bAd437e552AADF89f213c34eD97266160B06E0"  # vitalik.eth
 address2 = "0x9DDBbB468880906beb448c898c3F7d53F49F8144"
 
-fork_url = "https://avalanche-fuji-c-chain-rpc.publicnode.com"
+fork_url = os.environ.get('HTTPS_URL')
 
-# set up an evm
-evm = EVM(
-    # can fork from a remote node
-    fork_url=fork_url,
-    # can set tracing to true/false
-    #tracing=True,
-    # can configure the environment
-    # env=Env(
-    #     block=BlockEnv(timestamp=100)
-    # )
-)
-
+@timer_decorator
 def simulate_call():
+    # set up an evm
+    evm = EVM(
+        # can fork from a remote node
+        fork_url=fork_url,
+        # can set tracing to true/false
+        #tracing=True,
+        # can configure the environment
+        # env=Env(
+        #     block=BlockEnv(timestamp=100)
+        # )
+    )
 
     vb_before = evm.basic(address)
     print(vb_before)
@@ -55,5 +56,9 @@ def simulate_call():
     print(f"result onchain {int.from_bytes(result2, 'big')}")
 
 if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+    logging.basicConfig(level=logging.INFO)
+
     simulate_call()
 
